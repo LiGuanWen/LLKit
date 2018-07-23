db706f1c3cf6b2a9e5fe8ee40809c0f84a1e64fa
liguanwen <liguanwen@quhepai.com>
2018-07-23 19:43:48 +0800

    yyfpsLabel

---
+++ LLKitClass/LLUtility/YYFPSLabel.m
@@ -0,0 +1,169 @@
+//
+//  YYFPSLabel.m
+//  YYKitExample
+//
+//  Created by ibireme on 15/9/3.
+//  Copyright (c) 2015 ibireme. All rights reserved.
+//
+
+#import "YYFPSLabel.h"
+#import "YYKit.h"
+#import <sys/sysctl.h>
+#import <mach/mach.h>
+
+#define kSize CGSizeMake(55, 20)
+
+@implementation YYFPSLabel {
+    CADisplayLink *_link;
+    NSUInteger _count;
+    NSTimeInterval _lastTime;
+    UIFont *_font;
+    UIFont *_subFont;
+    
+    NSTimeInterval _llll;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (frame.size.width == 0 && frame.size.height == 0) {
+        frame.size = kSize;
+    }
+    self = [super initWithFrame:frame];
+    
+    self.layer.cornerRadius = 5;
+    self.clipsToBounds = YES;
+    self.textAlignment = NSTextAlignmentCenter;
+    self.userInteractionEnabled = NO;
+    self.backgroundColor = [UIColor colorWithWhite:0.000 alpha:0.700];
+    
+    _font = [UIFont fontWithName:@"Menlo" size:14];
+    if (_font) {
+        _subFont = [UIFont fontWithName:@"Menlo" size:4];
+    } else {
+        _font = [UIFont fontWithName:@"Courier" size:14];
+        _subFont = [UIFont fontWithName:@"Courier" size:4];
+    }
+    
+    _link = [CADisplayLink displayLinkWithTarget:[YYWeakProxy proxyWithTarget:self] selector:@selector(tick:)];
+    [_link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+    return self;
+}
+
+- (void)dealloc {
+    [_link invalidate];
+}
+
+- (CGSize)sizeThatFits:(CGSize)size {
+    return kSize;
+}
+
+- (void)tick:(CADisplayLink *)link {
+    if (_lastTime == 0) {
+        _lastTime = link.timestamp;
+        return;
+    }
+    
+    _count++;
+    NSTimeInterval delta = link.timestamp - _lastTime;
+    if (delta < 1) return;
+    _lastTime = link.timestamp;
+    float fps = _count / delta;
+    _count = 0;
+    
+    CGFloat progress = fps / 60.0;
+    UIColor *color = [UIColor colorWithHue:0.27 * (progress - 0.2) saturation:1 brightness:0.9 alpha:1];
+    
+    NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%d FPS",(int)round(fps)]];
+    
+    [text appendString:[NSString stringWithFormat:@"   %.2fMB",[self usedMemory]]];
+    [text appendString:[NSString stringWithFormat:@"   %.1f%% CPU",[self cpu_usage]]];
+
+    [text setColor:color range:NSMakeRange(0, text.length - 3)];
+    [text setColor:[UIColor whiteColor] range:NSMakeRange(text.length - 3, 3)];
+    text.font = _font;
+    [text setFont:_subFont range:NSMakeRange(text.length - 4, 1)];
+    
+    self.attributedText = text;
+}
+
+// 获取当前任务所占用的内存（单位：MB）
+- (double)usedMemory
+{
+    task_basic_info_data_t taskInfo;
+    mach_msg_type_number_t infoCount = TASK_BASIC_INFO_COUNT;
+    kern_return_t kernReturn = task_info(mach_task_self(),
+                                         TASK_BASIC_INFO,
+                                         (task_info_t)&taskInfo,
+                                         &infoCount);
+    
+    if (kernReturn != KERN_SUCCESS
+        ) {
+        return NSNotFound;
+    }
+    
+    return taskInfo.resident_size / 1024.0 / 1024.0;
+}
+
+//获取cpu
+-(float) cpu_usage
+{
+    kern_return_t kr;
+    task_info_data_t tinfo;
+    mach_msg_type_number_t task_info_count;
+    
+    task_info_count = TASK_INFO_MAX;
+    kr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)tinfo, &task_info_count);
+    if (kr != KERN_SUCCESS) {
+        return -1;
+    }
+    
+    task_basic_info_t      basic_info;
+    thread_array_t         thread_list;
+    mach_msg_type_number_t thread_count;
+    
+    thread_info_data_t     thinfo;
+    mach_msg_type_number_t thread_info_count;
+    
+    thread_basic_info_t basic_info_th;
+    uint32_t stat_thread = 0; // Mach threads
+    
+    basic_info = (task_basic_info_t)tinfo;
+    
+    // get threads in the task
+    kr = task_threads(mach_task_self(), &thread_list, &thread_count);
+    if (kr != KERN_SUCCESS) {
+        return -1;
+    }
+    if (thread_count > 0)
+        stat_thread += thread_count;
+    
+    long tot_sec = 0;
+    long tot_usec = 0;
+    float tot_cpu = 0;
+    int j;
+    
+    for (j = 0; j < thread_count; j++)
+    {
+        thread_info_count = THREAD_INFO_MAX;
+        kr = thread_info(thread_list[j], THREAD_BASIC_INFO,
+                         (thread_info_t)thinfo, &thread_info_count);
+        if (kr != KERN_SUCCESS) {
+            return -1;
+        }
+        
+        basic_info_th = (thread_basic_info_t)thinfo;
+        
+        if (!(basic_info_th->flags & TH_FLAGS_IDLE)) {
+            tot_sec = tot_sec + basic_info_th->user_time.seconds + basic_info_th->system_time.seconds;
+            tot_usec = tot_usec + basic_info_th->system_time.microseconds + basic_info_th->system_time.microseconds;
+            tot_cpu = tot_cpu + basic_info_th->cpu_usage / (float)TH_USAGE_SCALE * 100.0;
+        }
+        
+    } // for each thread
+    
+    kr = vm_deallocate(mach_task_self(), (vm_offset_t)thread_list, thread_count * sizeof(thread_t));
+    assert(kr == KERN_SUCCESS);
+    
+    return tot_cpu;
+}
+
+@end
